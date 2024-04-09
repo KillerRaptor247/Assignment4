6821f6a2a310e229fd83a4eed80c395d0dbe843d
unknown <mcar3550@students.kleinisd.net>
2024-04-09 15:27:11 -0500

    more fixes

--- frontend_deployment.bat
+++ frontend_deployment.bat
@@ -1,7 +1,7 @@
 docker image rm frontend
 docker build -t frontend ./nextjs
 minikube image load frontend
-kubectl delete service frontend
-kubectl delete deployment frontend
+kubectl delete service nextjs
+kubectl delete deployment nextjs
 kubectl apply -f ./nextjs/nextjs-deployment.yaml
 kubectl apply -f ./nextjs/nextjs-service.yaml
