302320d9128b2ae013aad07bb85a5efe1d8e2323
SecondPretender <peterm03cg@gmail.com>
2024-04-09 15:11:42 -0500

    added env file

--- backend_deployment.bat
+++ backend_deployment.bat
@@ -1,7 +1,13 @@
-docker image rm backend
-docker build -t backend ./fastapi
-minikube image load backend
-kubectl delete service backend
-kubectl delete deployment backend
+@set "filename=.env"
+
+for /F "delims== tokens=1,* eol=#" %%i in (%filename%) do @set %%i=%%~j
+
+if %BND_REBUILD% EQU true docker image rm backend
+if %BND_REBUILD% EQU true docker build -t backend ./fastapi
+if %BND_REBUILD% EQU true minikube image load backend
+
+if %BND_DELETE% EQU true kubectl delete service backend
+if %BND_DELETE% EQU true kubectl delete deployment backend
+
 kubectl apply -f ./fastapi/fastapi-deployment.yaml
 kubectl apply -f ./fastapi/fastapi-service.yaml
